9857820460718f65fdab79b217a256fa1e405fa1
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-04-23 09:47:16 -0700

    add --solo option to topaz options for running a smalltalk script using solo login; update help for exsting scripts and script creation ... make the script executable as part of creation step

--- bin/install.sh
+++ bin/install.sh
@@ -5,9 +5,12 @@
 
 set -e
 
+if [ ! -d "$ROWAN_PROJECTS_HOME" ] ; then
+	echo "expected environment variable \$ROWAN_PROJECTS_HOME to be defined"
+	exit 1
+fi
+
 stoneName="$1"
-shift
-stoneVers="$1"
 shift
 
 scriptFile=`realpath $0`
@@ -19,35 +22,43 @@ pushd  /usr/bin
 	fi
 	cd gemstone
 	if [ ! -e smalltalk ] ; then
-		sudo ln -s $scriptDir/smalltalk_350_interpretter smalltalk
-		sudo ln -s $scriptDir/topaz_350_interpretter topaz
+		sudo ln -s $scriptDir/smalltalk_350_interpreter smalltalk
+		sudo ln -s $scriptDir/topaz_350_interpreter topaz
 	fi
 popd
 
-if [ -d "$GS_HOME/server/stones/$stoneName" ] ; then
-	startStone -b $stoneName
-else
-	createStone -G $stoneName $stoneVers
-	cat - >> $GS_HOME/server/stones/${stoneName}/custom_stone.env << EOF
-	export ROWAN_PROJECTS_HOME=$GS_HOME/shared/repos
-EOF
-	stopNetldi $stoneName
-	startNetldi $stoneName
-fi
-
-export ROWAN_PROJECTS_HOME=$GS_HOME/shared/repos
-
-if [ ! -d "$GS_HOME/shared/repos/Rowan" ] ; then
-	pushd $GS_HOME/shared/repos
+if [ ! -d "$ROWAN_PROJECTS_HOME/Rowan" ] ; then
+	pushd $OWAN_PROJECTS_HOME
 		git clone https://github.com/GemTalk/Rowan.git
 		cd Rowan
 		git checkout candidateV2.0
 	popd
 fi
 
-$scriptDir/../scripts/install.tpz $stoneName -l
+if [ "$GS_HOME"x = "x" ] ; then
+	# GsDevKit_home
+	#		if $stoneName exists, then start it. Otherwise create a new stone
+	#
+	if [ -d "$GS_HOME/server/stones/$stoneName" ] ; then
+		startStone -b $stoneName
+	else
+		stoneVers="$1"
+		shift
+		createStone -G $stoneName $stoneVers
+	fi
+	cat - >> $GS_HOME/server/stones/${stoneName}/custom_stone.env << EOF
+export ROWAN_PROJECTS_HOME=$ROWAN_PROJECTS_HOME
+EOF
+	stopNetldi $stoneName
+	startNetldi $stoneName
 
-# create the solo extent
-snapshotDir="$Gs_HOME/server/stones/$stoneName/snapshots"
-$scriptDir/../scripts/snapshot.st --dir=$snapshotDir stash.dbf -- $stoneName -l
+	$scriptDir/../scripts/install.tpz $stoneName $topazArgs
+else
+	# GEMSTONE - expect the stone to be running
+	waitstone $stoneName
+
+	$scriptDir/../scripts/install.tpz $topazArgs
+fi
+
+
 
